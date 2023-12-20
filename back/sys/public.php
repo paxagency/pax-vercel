95ce9d63f41db87dcedfcfc48e39efea1f173813
“Albert <albert@paxagency.com>
2023-12-20 16:19:52 -0600

    Update public.php

--- back/sys/public.php
+++ back/sys/public.php
@@ -13,6 +13,6 @@ if($_URL[0]=='public') {
 	if($ext=="gif") header('Content-type: image/gif');
 	if($ext=="ico") header('Content-type: image/ico');
 	if($ext=="svg") header('Content-type: image/svg+xml');
-	return require_once($_CWD.$_URL_STRING);
+	return file_get_contents($_CWD.$_URL_STRING);
 }
 ?>
