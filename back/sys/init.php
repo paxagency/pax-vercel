a730fe83cd0e42e2af960e8ed7640147825568ee
“Albert <albert@paxagency.com>
2023-12-20 15:25:43 -0600

    test

--- back/sys/init.php
+++ back/sys/init.php
@@ -11,7 +11,13 @@ if(SITE_WWW && strpos($_SERVER['HTTP_HOST'],"www.")===false)
     header('Location: '.$_SERVER['REQUEST_SCHEME'].'://www.'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
 
 $_URL=array_slice(explode('/',parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH)),SITE_URL_INDEX);
-if($_URL[0]=='public') return require_once($_CWD."public/".$_URL[1]);
+if($_URL[0]=='public') {
+	$url =  implode("/",$_URL);
+	$ext = pathinfo($url, PATHINFO_EXTENSION);
+	if($ext=="css") header('Content-type: text/css');
+	return require_once($_CWD."public/".$_URL[1]);
+	}
+header('Content-type: text/css');
 $_PATH='error.html';
 $_PAGE = 'error';
 $path='';
